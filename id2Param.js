a754585f037e0b25376ffcfe284de07a237dd6e9
Miranda <cachehax@proton.me>
2023-07-02 20:37:52 -0700

    Delete id2Param.js

--- id2Param.js
+++
@@ -1,8 +0,0 @@
-// id2Param: cleans the value in the URL parameter and inserts it into HTML tag with matching ID.
-function id2Param(paramName, replaceID) {
-    fetchedParam = new URL(location.toLocaleString()).searchParams.get(paramName)
-    if (fetchedParam) {
-      let cleanString = fetchedParam.replace(/[^a-zA-Z 0-9 . -]/g, "")
-      document.getElementById(replaceID).innerHTML = cleanString
-    }
-}
